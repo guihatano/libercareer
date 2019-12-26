3aaea8fdf324d6b5689485451c41c0f9c27c09f3
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2019-12-26 12:01:51 -0300

    update home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,17 @@
 class HomeController < ApplicationController
   def index
     @answer_one = Rental.by_vehicle_type(:car).sum(:value)
+
+    @answer_two = ''
+    @answer_three = ''
+    @answer_four = ''
+    Rental.in_progress.each do |rental|
+      @answer_four += rental.person.full_name
+    end
+    @answer_five = ''
+    @answer_six = ''
+    @answer_seven = ''
+    @answer_eight = ''
+    @answer_nine = ''
   end
 end
