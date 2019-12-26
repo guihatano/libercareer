bac94e3f925a21fb30480093c9c426a01c761b8b
Guilherme Y. Hatano <guilherme@loldesign.com.br>
2019-12-26 10:39:38 -0300

    add enum to license on modalities column

--- app/models/license.rb
+++ app/models/license.rb
@@ -3,6 +3,19 @@
 class License < ApplicationRecord
   belongs_to :person
 
+  enum modalities: {
+    a: 'A',
+    b: 'B',
+    c: 'C',
+    d: 'D',
+    e: 'E',
+    ab: 'A,B',
+    ac: 'A,C',
+    ad: 'A,D',
+    ae: 'A,E',
+    special: 'special'
+  }
+
   def expired?
     expiration_date < Date.today
   end
